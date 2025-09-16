e83240e8611ed7dccf9530693281402a51eff218
SF Makgoba <makgobasf@gmail.com>
2025-09-16 23:04:01 +0200

    Completed the form handling for the registration page

--- registration.js
+++ registration.js
@@ -15,53 +15,125 @@ const departmentError = document.getElementById('departmentError');
 const startDateError = document.getElementById('startDateError');
 
 
-
 // function to valudate email using regex 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+const fullNameRegex = /^[A-Z][a-z]+(?: [A-Z][a-z]+)+$/;
 
 
 function validateEmail() {
     const email = emailInput.value.trim();
 
-    if (!emailRegex.test(email)) {
+    if (email === "") {
+        emailError.innerText = "This field is required";
+        emailInput.style.borderBottom = "1px solid red";
+        return false;
+    }
+    else if (!emailRegex.test(email)) { 
         emailError.innerText = "Please enter a valid email address";
         emailInput.style.borderBottom = "1px solid red";
         return false;
     } else {
         emailError.innerText = "";
-        emailInput.style.borderBottom = "";
+        emailInput.style.borderBottom = "1px solid green";
         return true;
     }
 }
 
+function validateFullName() { 
+    const fullNameInput = fullName.value.trim();
 
-
-function validateFullName() {
-    if (fullName.value.trim() === "") {
-        fullNameError.innerText = "Please enter a valid email address";
+    if (fullNameInput === "") {
+        fullNameError.innerText = "This field is required!";
+        fullName.style.borderBottom = "1px solid red";
+        return false;
+    } 
+    else if (!fullNameRegex.test(fullNameInput)) {
+        fullNameError.innerText = "Please enter a valid full name";
         fullName.style.borderBottom = "1px solid red";
         return false;
     } else {
-        fullNameError = "";
-        fullName.style.borderBottom = "";
+        fullNameError.innerText = "";
+        fullName.style.borderBottom = "1px solid green";
         return true;
     }
 }
 
-emailInput.addEventListener("blur", validateEmail); // validate form when the input loses focus
+function validatePosition() {
+    const positionInput = position.value.trim();
+
+    if (positionInput === ""){
+        positionError.innerText = "This field is required!";
+        position.style.borderBottom = "1px solid red";
+        return false;
+    } else {
+        positionError.innerText = "";
+        position.style.borderBottom = "1px solid green";
+        return true;
+    }
+}
+
+function validateDepartment() {
+    const departmentInput = department.value.trim();
+    
+    if (departmentInput === ""){
+        departmentError.innerText = "This field is required!";
+        department.style.borderBottom = "1px solid red";
+        return false;
+    } else {
+        departmentError.innerText = "";
+        department.style.borderBottom = "1px solid green";
+        return true; 
+    }
+}
+
+function validateStartDate() {
+    const startDateInput = startDate.value.trim();
+
+    if (startDateInput === ""){
+        startDateError.innerText = "This field is required!";
+        startDate.style.borderBottom = "1px solid red";
+        return false;
+    } else {
+        startDateError.innerText = "";
+        startDate.style.borderBottom = "1px solid green";
+        return true;
+    }
+}
+
+// validate form when each input loses focus 
+emailInput.addEventListener("blur", validateEmail); 
 fullName.addEventListener("blur", validateFullName);
-// add event listener to button
-button.addEventListener("click", (e) => {
+position.addEventListener("blur", validatePosition);
+department.addEventListener("blur", validateDepartment);
+startDate.addEventListener("blur", validateStartDate);
+
+// add event listener to button 
+form.addEventListener("submit", (e) => {
+
+    let valid = true;
 
     if (!validateEmail()) {
-        e.preventDefault();
+        valid = false;
     }
 
-    if (!validateFullName()){
+    if (!validateFullName()) {
+        valid = false;
+    }
+
+    if (!validatePosition()) {
+        valid = false;
+    }
+
+    if (!validateDepartment()) {
+        valid = false;
+    }
+
+    if (!validateStartDate()) {
+        valid = false;
+    }
+
+    if (!valid){
         e.preventDefault();
     }
-    //let hasError = false;
-    
-})
-
+});
